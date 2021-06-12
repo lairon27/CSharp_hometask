2cfeaab47f44f63c807e629468c8c2c26e6b5429
lairon27 <74205672+lairon27@users.noreply.github.com>
2021-06-12 15:59:09 +0300

    WinForms

---
+++ ConvetToPounds/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace ConvetToPounds
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(233, 48);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(241, 27);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(18, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Convert";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(233, 144);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(241, 27);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Visible = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {"Russian pound", "English pound", "Austrian pound"});
+            this.comboBox1.Location = new System.Drawing.Point(32, 102);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(157, 27);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Visible = false;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (177)))), ((int) (((byte) (216)))), ((int) (((byte) (183)))));
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.groupBox1.Location = new System.Drawing.Point(13, 32);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(200, 51);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.radioButton2.Location = new System.Drawing.Point(110, 12);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(84, 31);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "pounds";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.radioButton1.Location = new System.Drawing.Point(8, 12);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(97, 31);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "kilograms";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(18, 102);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 22);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "to";
+            this.label2.Visible = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Location = new System.Drawing.Point(13, 128);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(200, 51);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Visible = false;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.radioButton3.Location = new System.Drawing.Point(110, 12);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(84, 31);
+            this.radioButton3.TabIndex = 1;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "pounds";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.radioButton4.Location = new System.Drawing.Point(5, 12);
+            this.radioButton4.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(97, 31);
+            this.radioButton4.TabIndex = 0;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "kilograms";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (192)))), ((int) (((byte) (192)))), ((int) (((byte) (255)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(32, 252);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 27);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (192)))), ((int) (((byte) (192)))), ((int) (((byte) (255)))));
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(324, 252);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 27);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (177)))), ((int) (((byte) (216)))), ((int) (((byte) (183)))));
+            this.ClientSize = new System.Drawing.Size(490, 296);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Futura Md BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.Icon = ((System.Drawing.Icon) (resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Pounds/kilograms";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button button2;
+
+        private System.Windows.Forms.ToolTip toolTip1;
+
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+
+        private System.Windows.Forms.RadioButton radioButton2;
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+
+        private System.Windows.Forms.GroupBox groupBox1;
+
+        private System.Windows.Forms.ComboBox comboBox1;
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+
+        private System.Windows.Forms.TextBox textBox1;
+
+        #endregion
+    }
+}
---
+++ ConvetToPounds/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace ConvetToPounds
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.DrawMode = DrawMode.OwnerDrawFixed;
+            comboBox1.DrawItem += comboBox1_DrawItem;
+            comboBox1.DropDownClosed += comboBox1_DropDownClosed;
+        }
+        
+        private void comboBox1_DropDownClosed(object sender, EventArgs e)
+        {
+            toolTip1.Hide(comboBox1);
+        }
+        
+        private void comboBox1_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            if (e.Index < 0)
+            {
+                return;
+            }
+            var text = comboBox1.GetItemText(comboBox1.Items[e.Index]);
+            e.DrawBackground();
+            using (var br = new SolidBrush(e.ForeColor))
+            {
+                e.Graphics.DrawString(text, e.Font, br, e.Bounds);
+            }
+
+            if ((e.State & DrawItemState.Selected) == DrawItemState.Selected && comboBox1.Items[e.Index] == "Russian pound")
+            {
+                toolTip1.Show("1 російський фунт = 0,4095 кг", comboBox1, e.Bounds.Right, e.Bounds.Bottom);
+            }
+            if ((e.State & DrawItemState.Selected) == DrawItemState.Selected && comboBox1.Items[e.Index] == "English pound")
+            {
+                toolTip1.Show("1 англійський фунт = 0,453592 кг", comboBox1, e.Bounds.Right, e.Bounds.Bottom);
+            }
+            if ((e.State & DrawItemState.Selected) == DrawItemState.Selected && comboBox1.Items[e.Index] == "Austrian pound")
+            {
+                toolTip1.Show("1 австрійський фунт = 0,56001 кг", comboBox1, e.Bounds.Right, e.Bounds.Bottom);
+            }
+            e.DrawFocusRectangle();
+        }        
+        
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                comboBox1.Visible = true;
+                comboBox1.Location = new Point(32, 102);
+                label2.Location = new Point(13, 140);
+                groupBox2.Location = new Point(13, 160);
+                textBox2.Location = new Point(233, 180);
+                label2.Visible = true;
+                groupBox2.Visible = true;
+                textBox2.Visible = true;
+            }
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked)
+            {
+                comboBox1.Location = new Point(32, 199);
+                comboBox1.Visible = true;
+                label2.Visible = true;
+                groupBox2.Visible = true;
+                textBox2.Visible = true;
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                comboBox1.Visible = false;
+                label2.Visible = true;
+                groupBox2.Visible = true;
+                textBox2.Visible = true;
+                label2.Location = new Point(13, 102);
+                groupBox2.Location = new Point(13, 128);
+                textBox2.Location = new Point(233, 144);
+            }
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (radioButton4.Checked)
+                {
+                    if (comboBox1.SelectedItem == "Russian pound")
+                    {
+                        textBox2.Text = Convert.ToString(float.Parse(textBox1.Text) * 0.4095f);
+                    }
+
+                    if (comboBox1.SelectedItem == "English pound")
+                    {
+                        textBox2.Text = Convert.ToString(float.Parse(textBox1.Text) * 0.45359f);
+                    }
+
+                    if (comboBox1.SelectedItem == "Austrian pound")
+                    {
+                        textBox2.Text = Convert.ToString(float.Parse(textBox1.Text) * 0.56001f);
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Input pounds!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (radioButton1.Checked)
+                {
+                    if (comboBox1.SelectedItem == "Russian pound")
+                    {
+                        textBox2.Text = Convert.ToString(float.Parse(textBox1.Text) / 0.4095f, CultureInfo.InvariantCulture);
+                    }
+                    if (comboBox1.SelectedItem == "English pound")
+                    {
+                        textBox2.Text = Convert.ToString(float.Parse(textBox1.Text) / 0.45359f);
+                    }
+                    if (comboBox1.SelectedItem == "Austrian pound")
+                    {
+                        textBox2.Text = Convert.ToString(float.Parse(textBox1.Text) / 0.56001f);
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Input kilograms!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            try
+            {
+                if (radioButton4.Checked)
+                {
+                    if (comboBox1.SelectedItem == "Russian pound")
+                    {
+                        textBox2.Text = Convert.ToString(float.Parse(textBox1.Text) * 0.4095f);
+                    }
+                    if (comboBox1.SelectedItem == "English pound")
+                    {
+                        textBox2.Text = Convert.ToString(float.Parse(textBox1.Text) * 0.45359f);
+                    }
+                    if (comboBox1.SelectedItem == "Austrian pound")
+                    {
+                        textBox2.Text = Convert.ToString(float.Parse(textBox1.Text) * 0.56001f);
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Input pounds!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Location = new Point(13, 102);
+            groupBox2.Location = new Point(13, 128);
+            textBox2.Location = new Point(233, 144);
+            textBox1.Text = "";
+            textBox2.Text = "";
+            comboBox1.Text = "";
+            comboBox1.Visible = false;
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+            radioButton4.Checked = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
