083d95cf991d539dcc7b842fdc1a8bd078ac0017
lairon27 <74205672+lairon27@users.noreply.github.com>
2021-06-12 15:44:41 +0300

    Add files via upload

---
+++ TaskDelegate/Baggage.cs
@@ -0,0 +1,53 @@
+﻿namespace TaskDelegate
+{
+    public class Baggage
+    {
+        public delegate void BaggageStateHandler(string message);
+
+        private BaggageStateHandler _del;
+ 
+        public void RegisterHandler(BaggageStateHandler del)
+        {
+            _del += del; 
+        }
+        public void UnregisterHandler(BaggageStateHandler del)
+        {
+            _del -= del; 
+        }
+        
+        public string Surname { get; set; }
+        public int NumberOfThings { get; set; }
+        public float BaggageWeight{ get; set; }
+        
+        public Baggage( string surname, int num, float weight)
+        {
+            Surname = surname;
+            NumberOfThings = num;
+            BaggageWeight = weight;
+        }
+    
+        public override string ToString()
+        {
+            return Surname + " have " + NumberOfThings + " things by weight " + BaggageWeight + "kg";
+        }
+
+        public void permissibleWeight()
+        {
+            var permissible = 10f;
+            _del(BaggageWeight <= permissible
+                ? $"{Surname} has a permissible weight {BaggageWeight}kg of luggage"
+                : $"Luggage {Surname} exceeds the permissible weight {BaggageWeight}kg");
+        }
+        
+        public int moreThanTwoThings()
+        {
+            var amountOfPassengers = 0;
+            if (NumberOfThings > 2)
+            {
+                amountOfPassengers += 1;
+                _del($"{Surname} has more than 2 things - {NumberOfThings}");
+            }
+            return amountOfPassengers;
+        }
+    }
+}
---
+++ TaskDelegate/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace TaskDelegate
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var fileLines = File.ReadAllLines("1.txt");
+                var size = int.Parse(fileLines[0]);
+                var passengers = new Baggage[size];
+                
+                var count = 0;
+                foreach (var line in fileLines.Skip(1))
+                {
+                    var arr = line.Split(' ');
+                    int buf;
+                    float buf1;
+                    try
+                    {
+                        passengers[count] = new Baggage(arr[0], int.Parse(arr[1]), float.Parse(arr[2]));
+                        count++;
+                    }
+                    catch
+                    {
+                        if (!int.TryParse(arr[1],out buf))
+                        {
+                            Console.WriteLine( "Wrong value" );
+                            break;
+                        }
+
+                        if (!float.TryParse(arr[2], out buf1))
+                        {
+                            Console.WriteLine( "Wrong value" );
+                            break;
+                        }
+                    }
+                }
+                
+                foreach (var item in passengers)
+                    Console.WriteLine(item);
+                
+                Console.WriteLine();
+                var amountOfPassengers = 0;
+                var colorDelegate = new Baggage.BaggageStateHandler(Color_Message);
+                var showDelegate = new Baggage.BaggageStateHandler(Show_Message);
+                foreach (var t in passengers)
+                {
+                    t.RegisterHandler(colorDelegate);
+                    t.permissibleWeight();
+                }
+                
+                Console.WriteLine();
+                
+                foreach (var t in passengers)
+                {
+                    t.UnregisterHandler(colorDelegate);
+                    t.RegisterHandler(showDelegate);
+                    amountOfPassengers += t.moreThanTwoThings();
+                }
+               
+                Console.WriteLine();
+                
+                Console.WriteLine($"Total more than 2 things have {amountOfPassengers} passengers");
+            }
+            catch
+            {
+                Console.WriteLine("The file could not be read");
+            }
+        }
+        
+        private static void Color_Message(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+        
+        private static void Show_Message(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
